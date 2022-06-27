715f037dfab8caa8b6e40a856d2ae86e24d01da1
ThePursuitofCode <thepursuitofcode@gmail.com>
2022-06-27 12:01:18 -0600

    Completed UI for Tic Tac Toe board for The Odin Project.

---
+++ app.js
@@ -0,0 +1,26 @@
+// Goal have as little global code as possible!
+
+// Rule: If you need ONE of something (gameBoard, displayController) use a module.
+// Rule: If you need TWO+ of something (players), create them with factories.
+
+const gameBoard= (() = >{
+    
+})
+
+// Create a factory function object for the players
+
+// Create a factory function to control the flow of the game.
+
+// Create a function that allows players to add marks to a specific spot on the board. Don't allow players to play in spots already taken.
+
+// Build logic that checks if the game is over. Should check if 3 in a row and a tie. Display who the winner is on screen.
+
+// Add buttons for starting and restarting the game.
+
+// (Optional)
+// Build an AI so players can play against the computer.
+// - After player starts, AI chooses a random legal move.
+// - Possible to make an unbeatable AI by using the minimax algorithm.
+
+
+
